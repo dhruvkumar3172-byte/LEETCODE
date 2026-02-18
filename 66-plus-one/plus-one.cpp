6133784891022783f589c33ed2d275997902854f
dhruvkumar3172-byte <dhruvkumar3172@gmail.com>
2026-02-18 23:46:20 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (18.25%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
+        
+        int n = digits.size();
+       
 
-        for(int i = digits.size() - 1; i >= 0; i--){
-            if(digits[i] < 9){
-                digits[i]++;
-                return digits;
+        for(int i = n-1; i >= 0; i--){
+            
+            if(digits[i] == 9){
+                digits[i]=0;
+               
             }
             else{
-                digits[i] = 0;
+                digits[i] = digits[i] + 1;
+                return digits;
             }
+
+           
+
         }
          digits.insert(digits.begin(), 1);
         return digits;
-      
+        
+
     }
 };
