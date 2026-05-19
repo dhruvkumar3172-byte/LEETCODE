866c9ddc0169fa96ef888b99387f58f4e0d34f7f
dhruvkumar3172-byte <dhruvkumar3172@gmail.com>
2026-05-19 06:43:03 +0530

    Time: 744 ms (5.08%) | Memory: 714.9 MB (37.05%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+
+    bool searchRow(vector<int>rowIndex , int target){
+        int low = 0;
+        int high = rowIndex.size() - 1;
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+
+            if(target == rowIndex[mid]){
+                return true;
+            }
+            else if(target > rowIndex[mid]){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+         
+         return false;
+
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        int low = 0;
+        int high = row - 1;
+
+        for(int i = 0; i < row; i++){
+            if(target >= matrix[i][0] && target <= matrix[i][col - 1]){
+               
+               if(searchRow(matrix[i] , target)){
+                return true;
+               }
+            }
+        }
+        return false;
+        
+    }
+};
